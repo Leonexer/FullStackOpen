8ecdef0bf9ded9b2170657b1f3fb6f83945802f0
Alejandro Guzman <alejandroguzca@gmail.com>
2026-05-17 23:15:39 -0600

    Fixed WeatherInfo API calls

--- Part 2/country-data/src/components/Country.jsx
+++ Part 2/country-data/src/components/Country.jsx
@@ -2,8 +2,7 @@ import { useState } from 'react'
 import { useEffect } from 'react'
 
 export const Country = ({ country }) => {
-const api_key = import.meta.env.VITE_WEATHER_KEY
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${country.capital[0]}&appid=${api_key}&units=metric`
+const url = `https://api.open-meteo.com/v1/forecast?latitude=${country.latlng[0]}&longitude=${country.latlng[1]}&current_weather=true`
 
   const [weather, setWeather] = useState(null)
 
@@ -16,12 +15,12 @@ const url = `https://api.openweathermap.org/data/2.5/weather?q=${country.capital
                 throw new Error('Weather API error')
             }
         })
-        .then(data => setWeather(data))
+        .then(data => setWeather(data.current_weather))
         .catch(error => {
             console.error('Error fetching weather:', error)
             setWeather(null)
         })
-    }, [country.capital[0]])
+    }, [country.latlng[0], country.latlng[1]])
 
 
   return (
@@ -43,10 +42,9 @@ const url = `https://api.openweathermap.org/data/2.5/weather?q=${country.capital
         {
             weather &&
             <div>
-                <h2>Weather in {country.capital[0]}</h2>
-                <p>Temperature: {weather.main.temp} °C</p>
-                <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} />
-                <p>Wind: {weather.wind.speed} m/s</p>
+                <h2>Weather in {country.name.common}</h2>
+                <p>Temperature: {weather.temperature} °C</p>
+                <p>Wind: {weather.windspeed} m/s</p>
             </div>
          }
     </>
