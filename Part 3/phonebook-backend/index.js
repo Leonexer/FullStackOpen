3288270a6eb968e543d1759b20380be66cb013f0
Alejandro Guzman <alejandroguzca@gmail.com>
2026-05-19 20:20:02 -0600

    Exercise 3.16 done

--- Part 3/phonebook-backend/index.js
+++ Part 3/phonebook-backend/index.js
@@ -11,61 +11,67 @@ app.use(express.static('dist'))
 morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-app.get('/api/persons', (request, response) => {
+// GET
+app.get('/api/persons', (request, response, next) => {
   Person.find({}).then(persons => {
     response.json(persons)
-  })
+  }).catch(err => next(err))
 })
 
-// get
-app.get('/info', (request, response) => {
+// GET info
+app.get('/info', (request, response, next) => {
   Person.find({}).then(persons => {
     response.send(`
       <p>Phonebook has info for ${persons.length} people</p>
       <p>${new Date()}</p>
     `)
-  })
+  }).catch(err => next(err))
 })
 
-// get por id
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    if (person) {
-      response.json(person)
-    } else {
-      response.status(404).end()
-    }
-  })
+// GET por id
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(err => next(err))  // ← add this
 })
 
-// Delete
-app.delete('/api/persons/:id', (request, response) => {
-  Person.findByIdAndDelete(request.params.id).then(() => {
-    response.status(204).end()
-  })
+// DELETE
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(() => response.status(204).end())
+    .catch(err => next(err))  // ← add this
 })
 
 // POST (ahora a la base de datos)
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
+  if (!body.name) return response.status(400).json({ error: 'name is missing' })
+  if (!body.number) return response.status(400).json({ error: 'number is missing' })
 
-  if (!body.name) {
-    return response.status(400).json({ error: 'name is missing' })
-  }
-  if (!body.number) {
-    return response.status(400).json({ error: 'number is missing' })
-  }
-
-  const person = new Person({
-    name: body.name,
-    number: body.number,
-  })
-
-  person.save().then(savedPerson => {
-    response.json(savedPerson)
-  })
+  const person = new Person({ name: body.name, number: body.number })
+  person.save()
+    .then(savedPerson => response.json(savedPerson))
+    .catch(err => next(err))  // ← add this
 })
 
+// Error handler
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).json({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
