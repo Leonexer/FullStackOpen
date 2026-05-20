4703a53ac4910f38369751014cd289dc3c651aca
Alejandro Guzman <alejandroguzca@gmail.com>
2026-05-19 22:47:11 -0600

    Exercise 3.19 - 3.20 done

--- Part 3/phonebook-backend/models/person.js
+++ Part 3/phonebook-backend/models/person.js
@@ -15,7 +15,13 @@ const personSchema = new mongoose.Schema({
   },
   number: {
     type: String,
-    required: true
+    required: true,
+    validate: {
+      validator: (v) => {
+        return /^\d{2,3}-\d+$/.test(v) && v.length >= 8
+      },
+      message: props => `${props.value} is not a valid phone number! Format must be XX-XXXXXXX or XXX-XXXXXXX`
+    }
   }
 })
 
